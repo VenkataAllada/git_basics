cbc009cc8e01a28a08b6e5be8d218e422470c10f
VenkataAllada <venkata.allada403@gmail.com>
2024-09-30 19:56:31 +0000

    first python code

---
+++ hello.py
@@ -0,0 +1,15 @@
+# Print a message to the console
+print("Hello, world!")
+
+# Calculate the sum of two numbers
+a = 5
+b = 10
+sum = a + b
+print("The sum is:", sum)
+
+# Check if a number is even or odd
+num = 7
+if num % 2 == 0:
+    print(num, "is even")
+else:
+    print(num, "is odd")
